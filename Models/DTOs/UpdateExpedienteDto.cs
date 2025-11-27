2e151fc74716a69396e378758c47b3a3445368a4
Josue-Camacho <josuecamacho582@gmail.com>
2025-11-27 17:44:47 -0400

    feat:add UpdateExpedienteDto

---
+++ Models/DTOs/UpdateExpedienteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace final.Models.DTOs
+{
+    public class UpdateExpedienteDto
+    {
+        public string Codigo { get; set; }
+        public string DelitoPrincipal { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
