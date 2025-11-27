828ac1d9ada0f17c1d35c5991582a7dbcfd5343f
Josue-Camacho <josuecamacho582@gmail.com>
2025-11-27 18:02:53 -0400

    feat:add celda services

---
+++ Services/ICeldaService.cs
@@ -0,0 +1,14 @@
+﻿using final.Models;
+using final.Models.DTOs;
+
+namespace final.Services
+{
+    public interface ICeldaService
+    {
+        Task<IEnumerable<Celda>> GetAll();
+        Task<Celda?> GetOne(Guid id);
+        Task<Celda> CreateCelda(CreateCeldaDto dto);
+        Task<Celda> UpdateCelda(UpdateCeldaDto dto, Guid id);
+        Task DeleteCelda(Guid id);
+    }
+}
