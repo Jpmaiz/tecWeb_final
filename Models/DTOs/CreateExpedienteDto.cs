409f94868f427ef3279841eaf84221574b7f2b49
Josue-Camacho <josuecamacho582@gmail.com>
2025-11-27 17:44:11 -0400

    feat:add CreateExpedienteDto

---
+++ Models/DTOs/CreateExpedienteDto.cs
@@ -0,0 +1,10 @@
+﻿namespace final.Models.DTOs
+{
+    public class CreateExpedienteDto
+    {
+        public string Codigo { get; set; }
+        public string DelitoPrincipal { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public Guid ReclusoId { get; set; }
+    }
+}
