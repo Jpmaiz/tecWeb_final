eb776ccf8e181885ecfddc82c3fe2ce4eb75a86b
Josue-Camacho <josuecamacho582@gmail.com>
2025-12-19 00:38:41 -0400

    feat changes

--- Program.cs
+++ Program.cs
@@ -151,6 +151,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
