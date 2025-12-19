676a4757bd74fc847e0e3f2b922f33096ca01ac5
Josue-Camacho <josuecamacho582@gmail.com>
2025-12-19 00:18:27 -0400

    feat changes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -10,12 +10,27 @@ namespace final.Controllers
     {
         private readonly IUsuarioService _service;
 
-        public UsuarioController(IUsuarioService service)   
+        public UsuarioController(IUsuarioService service)
         {
             _service = service;
         }
 
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] CreateUsuarioDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-       
+            var usuario = await _service.Registrar(dto);
+            return Ok(usuario);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginUsuarioDto dto)
+        {
+            var token = await _service.Login(dto);
+            return Ok(new { token });
+        }
     }
+
 }
